25ef81cce7a763a757e7b0981def5cb31911b86c
Avotriniaina <avotriniaina24@gmail.com>
2024-08-06 11:06:55 +0200

    gerer mot de passe oublier

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,76 @@
 const express = require('express');
 const pool = require('../db');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const sendResetEmail = require('../utils/email')
+
+
+// Fonction pour générer un token de réinitialisation
+function generateResetToken() {
+  return crypto.randomBytes(20).toString('hex');
+}
+
+exports.forgotPassword = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    // Vérifier si l'email existe dans la base de données
+    const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    if (user.rows.length === 0) {
+      return res.status(404).json({ message: 'Utilisateur non trouvé.' });
+    }
+
+    // Générer un token de réinitialisation
+    const resetToken = generateResetToken();
+
+    let resetTokenExpires = new Date();
+    
+    // Ajoutez 15 minutes
+    resetTokenExpires.setMinutes(resetTokenExpires.getMinutes() + 15);
+
+    // Stocker le token et la date d'expiration dans la base de données
+    await pool.query('UPDATE users SET reset_password_token = $1, reset_password_expires = $2 WHERE email = $3', [resetToken, resetTokenExpires.toDateString(), email]);
+
+    // Envoyer l'email avec le lien de réinitialisation
+    await sendResetEmail(email, resetToken);
+
+    res.status(200).json({ message: 'Un lien de réinitialisation a été envoyé à votre email.' });
+  } catch (error) {
+    console.error('Erreur lors de la demande de réinitialisation:', error);
+    res.status(500).json({ message: 'Erreur lors de la demande de réinitialisation.' });
+  }
+}
+
+exports.resetPassword= async (req, res) => {
+  const { token } = req.params;
+  const { password } = req.body;
+
+  try {
+    // Obtenir la date actuelle au format ISO 8601
+    const currentTimestamp = new Date().toISOString();
+
+    // Vérifier si le jeton est valide et non expiré
+    const user = await pool.query(
+      'SELECT * FROM users WHERE reset_password_token = $1 AND reset_password_expires > $2',
+      [token, currentTimestamp]
+    );
+
+    if (user.rows.length === 0) {
+      return res.status(400).json({ message: 'Jeton invalide ou expiré.' });
+    }
+
+    // Mettre à jour le mot de passe en texte clair 
+    await pool.query(
+      'UPDATE users SET password = $1, reset_password_token = NULL, reset_password_expires = NULL WHERE id = $2',
+      [password, user.rows[0].id]
+    );
+
+    res.status(200).json({ message: 'Mot de passe réinitialisé avec succès.' });
+  } catch (error) {
+    console.error('Erreur lors de la réinitialisation du mot de passe:', error);
+    res.status(500).json({ message: 'Erreur lors de la réinitialisation du mot de passe.' });
+  }
+}
 
 exports.loginController = async (req, res) => {
   const { email, password } = req.body;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const {loginController, registerController} = require('../controllers/authController')
+const {loginController, registerController,forgotPassword, resetPassword} = require('../controllers/authController')
 
 const router = express.Router();
 
 router.post('/login', loginController)
 router.post('/register', registerController)
+
+// Route pour demander la réinitialisation du mot de passe
+router.post('/forgot-password',forgotPassword );
+  
+  router.post('/reset-password/:token', resetPassword);
+  
 
 module.exports = router
